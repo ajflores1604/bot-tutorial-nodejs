0fe505ba533bd5f39cea8201ae034654e8fd60b1
ajflores1604 <ajflores1604@gmail.com>
2016-11-17 21:09:00 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
+var sleep = require('sleep');
 
 var botID = process.env.BOT_ID;
 
@@ -26,12 +27,19 @@ function respond() {
   else if(request.text && botRegexSlut.test(request.text)) {
     this.res.writeHead(200);
     postMessage("Stop appropriating black culture1");
+    sleep.sleep(5)
     postMessage("Stop appropriating black culture2");
+    sleep.sleep(5)
     postMessage("Stop appropriating black culture3");
+    sleep.sleep(5)
     postMessage("Stop appropriating black culture4");
+    sleep.sleep(5)
     postMessage("Stop appropriating black culture5");
+    sleep.sleep(5)
     postMessage("Stop appropriating black culture6");
+    sleep.sleep(5)
     postMessage("Stop appropriating black culture7");
+    sleep.sleep(5)
     postMessage("Stop appropriating black culture8");
     this.res.end();
   } 
